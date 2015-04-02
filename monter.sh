33ece912662bdb4b5aa021b993448eee97fa7b41
pogodna36 <mar.brzoska.dom@gmail.com>
2015-04-02 16:23:16 +0200

    Update monter.sh

--- monter.sh
+++ monter.sh
@@ -11,8 +11,7 @@ cd ~
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 # echo "gem: --no-document" >> ~/.gemrc   
 \curl -sSL https://get.rvm.io | bash -s stable --rails
-#source /home/monter/.rvm/scripts/rvm
-source ~/.rvm/scripts/rvm
+source /home/monter/.rvm/scripts/rvm
 source ~/.bashrc
 rvm list
 gem install compass
